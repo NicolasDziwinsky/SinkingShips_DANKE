e9db6879b2a5d139228d326a905472a58b93126f
Nicolas <nicolas.dziwinsky@stud.fh-campuswien.ac.at>
2024-11-23 23:18:35 +0100

    Uploaded a demo project for java fx

---
+++ Demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+
+public class HelloController {
+    public Button exitButton;
+    public VBox Vbox;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public TextField textField;
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void showText(ActionEvent actionEvent) {
+        welcomeText.setText(textField.getText());
+    }
+
+
+    public void exit(ActionEvent actionEvent) {
+        stage = (Stage) Vbox.getScene().getWindow();
+
+        System.out.println("You successfully logged out!");
+
+        stage.close();
+
+    }
+
+
+
+}
+
