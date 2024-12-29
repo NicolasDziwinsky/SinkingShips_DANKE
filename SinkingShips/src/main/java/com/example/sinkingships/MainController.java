96d4c95a807a86e19c01e1c7128da52960d97055
nicolas <nicolas.dziwinsky@stud.fh-campuswien.ac.at>
2024-12-29 19:24:03 +0100

    Scene Switching implemented in Main Controller Class, and bound to the corresponding buttons

---
+++ SinkingShips/src/main/java/com/example/sinkingships/MainController.java
@@ -0,0 +1,51 @@
+package com.example.sinkingships;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void switchToMainMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToNewGame(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("NewGame.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToShipPlacement(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ShipPlacement.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToGame(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Game.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
